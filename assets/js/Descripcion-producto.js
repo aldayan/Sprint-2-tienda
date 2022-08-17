8d474f8a7940017fceacba73dcfefc72aae3a6f9
Aldayan <20187144@itla.edu.do>
2022-08-16 20:54:28 -0400

    descripcion

--- assets/js/Descripcion-producto.js
+++ assets/js/Descripcion-producto.js
@@ -1,25 +1,9 @@
 import { service } from "./service.js";
 
 
+
 //editando producto
 const formulario = document.querySelector('[data-form-descripcion]');
-
-
-
-const crearNueva = (url) => {
-
-
-    const tarjeta = document.createElement('div');
-
-
-    const conten = `
-    <div class="caja-motos" style="background-image:url(${url}); background-position: center; ">`
-
-    tarjeta.innerHTML = conten;
-
-    return tarjeta;
-};
-
 
 const obtenerInformacion = () => {
 
@@ -31,14 +15,14 @@ const obtenerInformacion = () => {
         window.location.href = "/index.html";
     }
 
-
+    const img = document.querySelector('[data-img-descripcion]');
     const nombreProducto = document.querySelector('[data-nombre]');
     const descripcion = document.querySelector('[data-descripcion]');
 
 
     service.detalleProducto(id).then((perfil) => {
 
-
+        img.value = perfil.url;
         nombreProducto.value = perfil.nombreProducto;
         descripcion.value = perfil.descripcion;
 
@@ -57,11 +41,12 @@ formulario.addEventListener('submit', (evento) => {
     const ur = new URL(window.location);
     const id = ur.searchParams.get("id");
 
+    const img = document.querySelector('[data-img-descripcion]').value;
     const nombreProducto = document.querySelector('[data-nombre]').value;
     const descripcion = document.querySelector('[data-descripcion]').value;
 
 
-    service.actualizarProducto(url, nombreProducto, descripcion, id).then(() => {
+    service.actualizarProducto(img, nombreProducto, descripcion, id).then(() => {
         window.location.href = '/administracion/administracion.html';
     })
 });
