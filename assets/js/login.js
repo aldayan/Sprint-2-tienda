07248360e320fed3ffcab89ee35a5b9b94f71bf2
Aldayan <20187144@itla.edu.do>
2023-01-05 19:28:12 -0400

    arreglos

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
-import { service } from "./service.js";
+/*import { service } from "./service.js";
 
 const formulario = document.querySelector('[data-form]');
 
@@ -10,8 +10,37 @@ formulario.addEventListener('submit', (evento) => {
     const email = document.querySelector('[data-email]').value;
     const contraseña = document.querySelector('[data-contraseña]').value;
 
+    var A = "aldayan";
+    var C = 123;
+    if (email.value == A && contraseña.value == C) {
+
+        window.location = 'administracion/administracion.html';
+    }
+
+
 
     service.crearSesion(email, contraseña).then(() => {
         window.location.href = '/administracion/administracion.html'
     }).catch(err => console.log(err))
-});
+
+}); */
+
+
+function ir() {
+
+    const email = document.querySelector('[data-email]').value;
+    const contraseña = document.querySelector('[data-contraseña]').value;
+
+    var A = "aldayan";
+    var C = 123;
+
+    if (email == A && contraseña == C) {
+
+        window.location.href = '../../administracion/administracion.html'
+
+    } else {
+
+        alert("usuario denegado")
+
+    }
+}
